cc30d8a98609541875a2e3ff0c7f539c6577447b
naveenelumalai <58645825+naveenelumalai@users.noreply.github.com>
2020-03-09 14:19:04 +0530

    Add files via upload

---
+++ second_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from second_app.models import CyberStories
+from second_app.models import NCRPComplaints
+
+# Register your models here.
+#admin.site.register(CyberStories)
+admin.site.register(NCRPComplaints)
